2403cc92a8cf625e181ca4adb58b44c9f0cbe87b
ANTHOX17 <antho852@outlook.es>
2024-10-11 23:24:45 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GameList from './GameList'; // Ajusta según la ruta real
+
+function App() {
+  return (
+    <div className="App">
+      <GameList />
+    </div>
+  );
+}
+
+export default App;
